c64a05630e46df0ea7ef6b188c4334365b48e7f2
Yuri Moreira Titi <yuri.titi@govbr.com.br>
2021-03-15 23:23:23 -0300

    ci(build): Ajuste rotina de deploy

--- devops/build.sh
+++ devops/build.sh
@@ -4,6 +4,12 @@ echo Limpando arquivos
 cd ..
 rm -rf build
 
+echo Build WEB
+cd web
+npm install --prefer-offline --no-audit --quiet
+npm run build
+cd ..
+
 echo Build API
 cd api
 npm install --prefer-offline --no-audit --quiet
